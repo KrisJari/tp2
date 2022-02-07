feef34e504eeb4b9c15daf1ce3191bec608637e6
KrisJari <98584832+KrisJari@users.noreply.github.com>
2022-02-07 09:20:08 +0100

    Add files via upload

---
+++ CityRoad.java
@@ -0,0 +1,11 @@
+package simulator.model;
+
+public class CityRoad extends Road{
+
+	CityRoad(String id, Junction srcJunct, Junction destJunc, int maxspeed, int contLimit, int length,
+			Weather weather) {
+		super(id, srcJunct, destJunc, maxspeed, contLimit, length, weather);
+		// TODO Auto-generated constructor stub
+	}
+
+}
