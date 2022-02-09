a69d26b983dbb33db28cf876ff4bdffb3c75a4c9
KrisJari <98584832+KrisJari@users.noreply.github.com>
2022-02-10 00:22:39 +0100

    cambio build

--- SimuladorTrafico/src/simulator/model/CityRoad.java
+++ SimuladorTrafico/src/simulator/model/CityRoad.java
@@ -8,4 +8,22 @@ public class CityRoad extends Road{
 		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	void reduceTotalContamination() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void updateSpeedLimit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	int calculateVehicleSpeed(Vehicle v) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
