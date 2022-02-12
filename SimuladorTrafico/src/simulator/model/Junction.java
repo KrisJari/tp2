4ff69d8086ea3d2861758a71968ae3bc0ef76506
KrisJari <98584832+KrisJari@users.noreply.github.com>
2022-02-12 19:29:31 +0100

    road report habia que hacer un foreach para meter la lista de vehicles y junction estoy haciendo addincomingroad

--- SimuladorTrafico/src/simulator/model/Junction.java
+++ SimuladorTrafico/src/simulator/model/Junction.java
@@ -14,10 +14,11 @@ public class Junction extends SimulatedObject{
   private List<Road> road;
   private  Map<Junction,Road> mapRoad;
   private List<List<Vehicle>> colavehicles;
+  private Map<Road,List<Vehicle>> mapaColas;
   private int indSV;
   private int ultCamS;
-  private LightSwitchingStrategy round;
-  private DequeuingStrategy first ;
+  private LightSwitchingStrategy isStrategy;
+  private DequeuingStrategy dqStrategy ;
   private int x;
   private int y;
   
@@ -27,7 +28,22 @@ public class Junction extends SimulatedObject{
 		this.road=new ArrayList<>();
         this.mapRoad=new HashMap<Junction,Road>() ;
 		this.colavehicles= new ArrayList<List<Vehicle>>();
-        
+        if(isStrategy==null||dqStrategy==null)
+		{
+            throw new IllegalArgumentException("Las estrategias son nulas");
+		}
+		this.dqStrategy=dqStrategy;
+		this.isStrategy=isStrategy;
+
+		if(xCoor<0||yCoor<0)
+		{
+			throw new IllegalArgumentException("Las coordenadas son negativas");
+
+		}
+		this.x=xCoor;
+        this.y=yCoor;
+
+		this.ultCamS=1;
 	}
     void enter(Vehicle v)
 	{
@@ -40,13 +56,17 @@ public class Junction extends SimulatedObject{
 		   throw new IllegalArgumentException("no es una carretera entrante");
 	   }
 	   road.add(r);
-	   LinkedList colaR=new LinkedList();
-	   colaR.add(r);
-
 	   //me creo una cola de roads y luego me creo un mapa con esas colas y Road 
 	   //que añade ese mapa al par de Map<Junction ,Road>
 
 	   //Map<Road,ColaR>?
+
+	   //me creo una lista de colas según la página 12 List<List<Vehicles>> pero como es una la declaramos
+	   List<Vehicle> cola= new LinkedList<>();
+	   //añadimos la final de la lista de colas
+	   colavehicles.add(cola);
+	   //el mapa carretera cola es mapaColas pagina 12 se dice que se guarde para hacer la búsqueda
+	   
    }
 protected Road roadTo (Junction j)
 {
