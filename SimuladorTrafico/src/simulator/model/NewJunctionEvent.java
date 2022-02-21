771b2525d2df9bf621ccc4789798d29b33a86bba
valentina-baseotto <91380643+valentina-baseotto@users.noreply.github.com>
2022-02-21 10:46:36 +0100

    NewJunctionEvent.java terminado

--- SimuladorTrafico/src/simulator/model/NewJunctionEvent.java
+++ SimuladorTrafico/src/simulator/model/NewJunctionEvent.java
@@ -1,17 +1,20 @@
 package simulator.model;
 
 public class NewJunctionEvent extends Event{
+	
+	private Junction junct;
 
     public NewJunctionEvent(int time,String id,LightSwitchingStrategy iStrategy,DequeuingStrategy dStrategy,int xCoor,
     int yCoor) {
         super(time);
         //TODO Auto-generated constructor stub
+        this.junct = new Junction (id,iStrategy,dStrategy,xCoor,yCoor);
     }
 
     @Override
     public void execute(RoadMap map) {
         // TODO Auto-generated method stub
-        
+        map.addJunction(this.junct);
     }
     
 }
