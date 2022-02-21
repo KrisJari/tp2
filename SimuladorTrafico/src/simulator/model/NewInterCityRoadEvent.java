234d869a92d6ce6ca22320ca9932e39e6e9e8e75
valentina-baseotto <91380643+valentina-baseotto@users.noreply.github.com>
2022-02-21 16:58:34 +0100

    NewInterCityRoad.java hecho

--- SimuladorTrafico/src/simulator/model/NewInterCityRoadEvent.java
+++ SimuladorTrafico/src/simulator/model/NewInterCityRoadEvent.java
@@ -1,16 +1,19 @@
 package simulator.model;
 
 public class NewInterCityRoadEvent extends Event  {
+	
+	private Road interCity;
 
-    NewInterCityRoadEvent(int time) {
+    NewInterCityRoadEvent(int time, String id, Junction srcJun, Junction destJunc, int length, int co2Limit, int maxSpeed, Weather weather) {
         super(time);
         //TODO Auto-generated constructor stub
+        this.interCity = new InterCityRoad(id, srcJun, destJunc,length, co2Limit,maxSpeed,weather);
     }
 
     @Override
     public void execute(RoadMap map) {
         // TODO Auto-generated method stub
-        
+        map.addRoad(this.interCity);
     }
     
 }
