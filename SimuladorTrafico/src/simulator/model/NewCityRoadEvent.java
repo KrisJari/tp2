c0853c1a276c35dc04753b2a45f35931bb8109c0
valentina-baseotto <91380643+valentina-baseotto@users.noreply.github.com>
2022-02-21 16:55:01 +0100

    NewCityRoad.java hecho

--- SimuladorTrafico/src/simulator/model/NewCityRoadEvent.java
+++ SimuladorTrafico/src/simulator/model/NewCityRoadEvent.java
@@ -1,16 +1,19 @@
 package simulator.model;
 
 public class NewCityRoadEvent extends Event {
-
-    NewCityRoadEvent(int time) {
+	
+	private CityRoad cityRoad; 
+	
+    public NewCityRoadEvent(int time, String id, Junction srcJun, Junction destJunc, int lenght, int co2Limit, int maxSpeed, Weather weather) {
         super(time);
         //TODO Auto-generated constructor stub
+        this.cityRoad = new CityRoad(id, srcJun, destJunc, maxSpeed, co2Limit, lenght, weather);
     }
 
     @Override
     public void execute(RoadMap map) {
         // TODO Auto-generated method stub
-        
+        map.addRoad(this.cityRoad);
     }
     
 }
