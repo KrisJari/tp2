9c8468f80db954116aca99c398049b7195f11ee8
valentina-baseotto <91380643+valentina-baseotto@users.noreply.github.com>
2022-03-02 21:22:37 +0100

    Mas Builders y corrección en NewVehicleEvent

--- SimuladorTrafico/src/simulator/model/NewInterCityRoadEvent.java
+++ SimuladorTrafico/src/simulator/model/NewInterCityRoadEvent.java
@@ -3,7 +3,7 @@ package simulator.model;
 public class NewInterCityRoadEvent extends NewRoadEvent {
 	
 
-    NewInterCityRoadEvent(int time, String id, String srcJun, String destJunc, int length, int co2Limit, int maxSpeed, Weather weather) {
+    public NewInterCityRoadEvent(int time, String id, String srcJun, String destJunc, int length, int co2Limit, int maxSpeed, Weather weather) {
         super(time, id, srcJun, destJunc, length, maxSpeed, weather);
         //TODO Auto-generated constructor stub
     }
--- SimuladorTrafico/src/simulator/model/NewVehicleEvent.java
+++ SimuladorTrafico/src/simulator/model/NewVehicleEvent.java
@@ -1,21 +1,43 @@
 package simulator.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NewVehicleEvent extends Event{
 	
 	private Vehicle vehicle;
+	private String id;
+	private int maxSpeed;
+	private int contClass;
+	private List<String> itinerary;
 
-    NewVehicleEvent(int time, String id, int maxSpeed, int contClass, List<Junction> itinerary) {
+    public NewVehicleEvent(int time, String id, int maxSpeed, int contClass, List<String> itinerary) {
         super(time);
         //TODO Auto-generated constructor stub
-        this.vehicle = new Vehicle(id, maxSpeed, contClass, itinerary);
+        
+        if(time < 0) throw new IllegalArgumentException("Time can't be negative");
+		if(maxSpeed < 0) throw new IllegalArgumentException("Max Speed must be positive number");
+		if(contClass < 0 || contClass > 10) throw new IllegalArgumentException("ContClass must be a value between 0 and 10");
+		
+        
+        this.id = id;
+        this.maxSpeed = maxSpeed;
+        this.contClass = contClass;
+        this.itinerary = itinerary;
     }
 
     @Override
     void execute(RoadMap map) {
         // TODO Auto-generated method stub
+    	List<Junction> jItinerary = new ArrayList<Junction>();
+    	for (String i : this.itinerary) {
+    		Junction j = map.getJunction(i);
+    		jItinerary.add(j);
+    	}
+    	
+    	this.vehicle = new Vehicle(id,maxSpeed,contClass,jItinerary);
         map.addVehicle(this.vehicle);
+        vehicle.moveToNextRoad();
     }
     
 }
